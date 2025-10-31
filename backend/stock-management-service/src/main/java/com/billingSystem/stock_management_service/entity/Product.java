7e4ef03490dab3dc4eb0c33c51c31e01140af022
Pericharla-Likhitha <likhitha.pericharla.17@gmail.com>
2025-10-31 14:57:35 -0500

    stock-management backend code changes

---
+++ backend/stock-management-service/src/main/java/com/billingSystem/stock_management_service/entity/Product.java
@@ -0,0 +1,23 @@
+package com.billingSystem.stock_management_service.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int stockQuantity;
+    private int minStockLevel;
+    private String emailForAlert;
+}
+
