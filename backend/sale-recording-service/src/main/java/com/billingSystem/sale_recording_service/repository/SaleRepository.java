a3fed2f688accd2696ddb21f73c5ac9894cdd865
Pericharla-Likhitha <likhitha.pericharla.17@gmail.com>
2025-11-05 23:42:44 -0600

    Revert "sale recording service"
    This reverts commit d6515c8030ac98286ce2f1819b93ab77660a9b8d.

--- backend/sale-recording-service/src/main/java/com/billingSystem/sale_recording_service/repository/SaleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.billingSystem.sale_recording_service.repository;
-
-import com.billingSystem.sale_recording_service.entity.Sale;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface SaleRepository extends JpaRepository<Sale, UUID> {
-}
--- backend/sale-recording-service/src/main/java/com/billingSystem/sale_recording_service/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.billingSystem.sale_recording_service.repository;
-
-import com.billingSystem.sale_recording_service.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface UserRepository extends JpaRepository<User, UUID> {
-}
